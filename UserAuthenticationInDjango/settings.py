5f44689d40b7046ab84d07de5ec7208080a13d9e
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-03-08 20:42:47 +0500

    Tiny configuration of template and url in django.

--- UserAuthenticationInDjango/settings.py
+++ UserAuthenticationInDjango/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'UserAuthenticationInDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login & Logout URLs
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/home/'
+LOGOUT_REDIRECT_URL = '/login/'
