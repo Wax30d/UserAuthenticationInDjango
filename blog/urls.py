2e6c10cf827f2d16750652c08ad47ffcfcdf188c
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-03-09 08:12:31 +0500

    Created blog's url.

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+]
