58e11e6d639817e1988a38ddc0ebae6306af61b7
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-03-09 08:33:39 +0500

    DB migration and forget to import path.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import views as auth_views
 from . import views
+from django.urls import path
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
